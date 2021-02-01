2b197727fb6a1ea77eed28978a1d7435a08cf6df
Daksh Jain <dakshbjain@gmail.com>
2021-02-01 16:41:29 -0500

    added more file types

--- sort_files.py
+++ sort_files.py
@@ -15,20 +15,20 @@ def file_extension(file_name):
 
 # Folders to create/place into
 file_types = ['/exe', '/doc', '/pdf', '/images',
-              '/misc', '/zip', '/sheets', '/video']
+              '/misc', '/zip', '/sheets', '/video', '/installers']
 
 for ftype in file_types:
     if not os.path.exists(sort_path + ftype):
         os.makedirs(sort_path + ftype)
 
 type_mappings = {'png': 'images', 'jpg': 'images', 'jpeg': 'images', 'pdf': 'pdf', 'exe': 'exe',
-                 'doc': 'doc', 'docx': 'doc', 'zip': 'zip', 'rar': 'zip', 'mp4': 'video'}
+                 'doc': 'doc', 'docx': 'doc', 'zip': 'zip', 'rar': 'zip', 'mp4': 'video', 'msi': 'installers'}
 
 for file_name in os.listdir(sort_path):
     extension = file_extension(file_name)
     folder_name = type_mappings.get(extension, None)
+    old_path = [sort_path, file_name]
     if folder_name is None:
         continue
-    old_path = [sort_path, file_name]
     new_path = [sort_path, folder_name, file_name]
     shutil.move("/".join(old_path), "/".join(new_path))
